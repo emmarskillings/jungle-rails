5488671ff17ad32a72630cec65debab823323708
emmarskillings <emmarskillings@gmail.com>
2018-07-19 22:15:37 +0000

    confirmation email now created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,11 +10,13 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
+      OrderMailer.order_confirmation(order).deliver_now
+
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
- 
+
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
