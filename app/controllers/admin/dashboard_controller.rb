2845946f7605e1a83e79158e86873eb6a1981072
emmarskillings <emmarskillings@gmail.com>
2018-07-19 22:30:45 +0000

    must be admin to access admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class Admin::DashboardController < ApplicationController
 
+  http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
+
   before_filter :authorize
 
   def show
   end
-  
+
 end
