0cca74fb9f8b59fedff72f71dcdd9a0b59e83107
emmarskillings <emmarskillings@gmail.com>
2018-07-23 17:34:52 +0000

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :reviews
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 end
